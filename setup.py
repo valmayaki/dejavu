1e378e7fb3071c3e7e626a73192b11289ee6e891
Valentine Ubani Mayaki <mxvmayaki@gmail.com>
2020-04-23 17:10:24 +0100

    Updates setup.py to fix install error (#1)
    * Updates setup.py to fix install error
    
    * reduce requirement file dependencies
    
    * change setup description

--- setup.py
+++ setup.py
@@ -1,32 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 # import os, sys
 
-
-def is_package(path):
-    return (
-        os.path.isdir(path) and
-        os.path.isfile(os.path.join(path, '__init__.py'))
-    )
-
-
-def find_packages(path, base=''):
-    """Find all packages in path """
-    packages = {}
-    for item in os.listdir(path):
-        dir = os.path.join(path, item)
-        if is_package(dir):
-            if base:
-                module_name = base + '.' + item
-            else:
-                module_name = item
-            packages[module_name] = dir
-            packages.update(find_packages(dir, module_name))
-    return packages
-
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
 
 def parse_requirements(requirements):
     # load from requirements.txt
@@ -66,12 +40,12 @@ setup(
     long_description=DESCRIPTION,
     author='Will Drevo',
     author_email='will.drevo@gmail.com',
-    maintainer="Will Drevo",
-    maintainer_email="will.drevo@gmail.com",
-    url='http://github.com/tuxdna/dejavu',
+    maintainer="Valentine Mayaki",
+    maintainer_email="mxvmayaki@gmail.com",
+    url='http://github.com/valmayaki/dejavu',
     license='MIT License',
     include_package_data=True,
-    packages=find_packages("."),
+    packages=find_packages(),
     platforms=['Unix'],
     install_requires=REQUIREMENTS,
     classifiers=[
